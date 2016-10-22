f941dc634300fac6e0db9b7470860e3d504f020f
James Valleroy <jvalleroy@mailbox.org>
2016-10-22 15:21:10 -0400

    New upstream version 0.8.0~dfsg1

---
+++ plugins/qrcode/qrcode.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin qrcode
+ * Add QRCode containing URL for each links.
+ * Display a QRCode icon in link list.
+ */
+
+/**
+ * Add qrcode icon to link_plugin when rendering linklist.
+ *
+ * @param array $data - linklist data.
+ *
+ * @return mixed - linklist data with qrcode plugin.
+ */
+function hook_qrcode_render_linklist($data)
+{
+    $qrcode_html = file_get_contents(PluginManager::$PLUGINS_PATH . '/qrcode/qrcode.html');
+
+    foreach ($data['links'] as &$value) {
+        $qrcode = sprintf($qrcode_html,
+            urlencode($value['url']),
+            $value['url'],
+            PluginManager::$PLUGINS_PATH
+        );
+        $value['link_plugin'][] = $qrcode;
+    }
+
+    return $data;
+}
+
+/**
+ * When linklist is displayed, include qrcode JS files.
+ *
+ * @param array $data - footer data.
+ *
+ * @return mixed - footer data with qrcode JS files added.
+ */
+function hook_qrcode_render_footer($data)
+{
+    if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
+        $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/qrcode/shaarli-qrcode.js';
+    }
+
+    return $data;
+}
+
+/**
+ * When linklist is displayed, include qrcode CSS file.
+ *
+ * @param array $data - header data.
+ *
+ * @return mixed - header data with qrcode CSS file added.
+ */
+function hook_qrcode_render_includes($data)
+{
+    if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
+        $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/qrcode/qrcode.css';
+    }
+
+    return $data;
+}
